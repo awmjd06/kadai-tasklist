35395615077de30e41b45386846cf2c4c676a136
mai.oono <lilac.jpg@gmail.com>
2019-08-09 07:32:23 +0000

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
 
-  
-    
   get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
- # get 'sessions/new'
-#  get 'sessions/create'
-#  get 'sessions/destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:new, :create]
